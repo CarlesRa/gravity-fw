1e63e1bc3cb5c1fc4ef570fd5dd6c9c6c085cae6
CarlesRa <jcramos8282@gmail.com>
2021-09-25 23:51:55 +0200

    start framework structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {extensionsRoutes} from "./extensions/extensions-routing.module";
 
-const routes: Routes = [];
+const routes: Routes = extensionsRoutes;
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
